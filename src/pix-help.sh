d441f118baeda26bc6e0fb7c7cd912442c678e38
progwi0 <93661506+progwi0@users.noreply.github.com>
2025-01-12 10:16:46 +0200

    Delete src/pix-help.sh

--- src/pix-help.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "pix-help:"
-echo "pix-inst - install bin from pix"
-echo "pix-remove - remove bin from pix"
-echo "pix-run - run bin from pix"
-echo "pix-uninst - uninstall pix"
